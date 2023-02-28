a29eb2d6946aa0253d75421f394a3ffa29fa5875
Rocco Piccirillo <piccirillo.rocco@gmail.com>
2023-02-28 00:51:53 -0500

    Day 31 Complete
    Completed a working flash card app. The cards are currently for learning basic words from French to English. Will eventually change cards to something else

--- Day-31/main.py
+++ Day-31/main.py
@@ -3,38 +3,39 @@ from tkinter import *
 import pandas
 import random
 BACKGROUND_COLOR = "#B1DDC6"
-FRENCH_WORD = ""
-FRENCH_LIST = []
-ENGLISH_WORD = ""
-current_num = None
 current_card = {}
+
 #------Generate new word----
-word_list = pandas.read_csv("./data/french_words.csv")
-to_learn = word_list.to_dict(orient="records")
+try:
+    file = open("./data/words_to_learn.csv","r")
+except FileNotFoundError:
+    file = open("./data/french_words.csv","r")
+#word_list = pandas.read_csv("./data/french_words.csv")
+finally:
+    word_list = pandas.read_csv(file)
+    to_learn = word_list.to_dict(orient="records")
+    file.close()
 #print(french)
-def random_french_word():
-    #global FRENCH_WORD
-    global FRENCH_LIST
-    #global current_num
+def next_card():
     global current_card, flip_timer
-
-    #current_num = random.randint(0,len(word_list)-1)
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    #FRENCH_WORD = (word_list["French"][current_num])
-
-    #if FRENCH_WORD in FRENCH_LIST:
-        #random_french_word()
-    #else:
     canvas.itemconfig(index_card, image=front_flash)
     canvas.itemconfig(vocab, text=current_card["French"],fill="#000000")
     canvas.itemconfig(top_text, text='French',fill="#000000")
-        #FRENCH_LIST.append(FRENCH_WORD)
+    flip_timer = window.after(3000,func=flip_card)
 
 
 def flip_card():
     canvas.itemconfig(index_card, image=back_flash)
     canvas.itemconfig(vocab, text=current_card["English"], fill="#FAFAEB")
     canvas.itemconfig(top_text, text='English', fill="#FAFAEB")
+
+def known_card():
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv",index = False)
+    next_card()
 
 window = Tk()
 window.title("Flashcard")
@@ -52,11 +53,12 @@ canvas.pack()
 
 
 wrong_image = PhotoImage(file="./images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0,command=random_french_word)
+wrong_button = Button(image=wrong_image, highlightthickness=0,command=next_card)
 wrong_button.pack(side="left")
 
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0,command=known_card)
 right_button.pack(side = "right")
-random_french_word()
+next_card()
 window.mainloop()
+#unknown_cards()
